2f3dbbe63a88a74ebff33dfdc65725f79ee18573
Syed Turaab Haider Kazmi <syedzainkazmi78@gmail.com>
2026-01-06 20:37:34 +0500

    Add files via upload

---
+++ googleConsentForm/google_consent_obj/GoogleConsentManager.kt
@@ -0,0 +1,108 @@
+package com.kazmi.dev.my.secret.media.ads_consent.google_consent_obj
+
+import android.app.Activity
+import android.content.Context
+import android.util.Log
+import com.google.android.gms.ads.MobileAds
+import com.google.android.ump.ConsentDebugSettings
+import com.google.android.ump.ConsentInformation
+import com.google.android.ump.ConsentRequestParameters
+import com.google.android.ump.UserMessagingPlatform
+
+object GoogleConsentManager {
+
+    private const val TAG = "consent_info_324786235923"
+
+    private lateinit var consentInfo: ConsentInformation
+    private var shouldCheckCanRequestAds: Boolean = true
+
+    /** Helper variable to determine if the privacy options form is required. */
+    val isPrivacyOptionsRequired: Boolean
+        get() =
+            consentInfo.privacyOptionsRequirementStatus ==
+                    ConsentInformation.PrivacyOptionsRequirementStatus.REQUIRED
+
+    fun initConsentInfo(
+        activity: Activity,
+        debugMode: Boolean = false,
+        onAdsInitialized: () -> Unit,
+        onError: (String) -> Unit
+    ) {
+
+        /**Consent Info*/
+        consentInfo = UserMessagingPlatform.getConsentInformation(activity)
+
+        /** Consent Request Builder */
+        val consentRequest = ConsentRequestParameters.Builder()
+            .apply {
+                if (debugMode) {
+                    val debugSettings = ConsentDebugSettings.Builder(activity)
+                        .setDebugGeography(ConsentDebugSettings.DebugGeography.DEBUG_GEOGRAPHY_EEA)
+                        .build()
+                    setConsentDebugSettings(debugSettings)
+                }
+            }
+            .build()
+
+        /** Request Consent */
+        consentInfo.requestConsentInfoUpdate(
+            activity,
+            consentRequest,
+            {/*handle Success*/
+
+                Log.d(TAG, "initConsentInfo: Consent info gathered Success.")
+
+                if (consentInfo.canRequestAds()) {
+                    //handle ads initialization
+                    Log.d(TAG, "initConsentInfo: Yes can request Ads first check.")
+                    shouldCheckCanRequestAds = false
+                    initializeAds(activity.applicationContext, onAdsInitialized)
+                }
+
+                UserMessagingPlatform.loadAndShowConsentFormIfRequired(activity) { formError ->
+                    if (formError != null) {
+                        //handle Error
+                        val errorMessage = formError.message
+                        Log.d(TAG, "initConsentInfo: Consent form Error -> $errorMessage")
+                        onError(errorMessage)
+                    } else {
+                        //handle ads initialization
+                        Log.d(TAG, "initConsentInfo: Consent form Success.")
+                        if (shouldCheckCanRequestAds) {
+                            Log.d(TAG, "initConsentInfo: Yes can request Ads second check.")
+                            initializeAds(activity.applicationContext, onAdsInitialized)
+                        }
+                    }
+                }
+
+            },
+            { formError ->  /*handle Error*/
+                Log.d(TAG, "initConsentInfo: Consent info gathered Error -> ${formError.message}.")
+            }
+        )
+
+    }
+
+    private fun initializeAds(
+        context: Context,
+        onAdsInitialized: () -> Unit,
+    ) {
+        MobileAds.initialize(context) {
+            //load required ads
+            Log.d(TAG, "initializeAds: Ads Initialized")
+            onAdsInitialized()
+        }
+    }
+
+    fun showPrivacyOptionForm(activity: Activity){
+        UserMessagingPlatform.showPrivacyOptionsForm(activity) { formError->
+            if (formError != null){
+                //handle error
+                Log.d(TAG, "showPrivacyOptionForm: Error -> ${formError.message}")
+            }else{
+                Log.d(TAG, "showPrivacyOptionForm: Success")
+            }
+        }
+    }
+
+}
